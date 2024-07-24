ddd9f11475738f917586b66b74b675e83617a82c
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 13:19:09 +0530

    Update useArticleQuery.js

--- src/hooks/useArticleQuery.js
+++ src/hooks/useArticleQuery.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 
 const getArticleBySlug = async (slug) => {
-    const {data} = await axios.get(`http://localhost:3001/api/articles/${slug}`);
+    const {data} = await axios.get(`https://bloggingwebsite-backend-a7q5.onrender.com/api/articles/${slug}`);
   
     console.log("getCurrentUser", { data });
   
