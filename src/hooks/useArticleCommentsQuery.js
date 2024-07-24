45c4e1762b9fcf311f6c7fa4f2772ab67ed851e8
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 13:23:00 +0530

    Update useArticleCommentsQuery.js

--- src/hooks/useArticleCommentsQuery.js
+++ src/hooks/useArticleCommentsQuery.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 const getArticleComments = async (slug) => {
     const { data } = await axios.get(
-        `http://localhost:3001/api/articles/${slug}/comments`
+        `https://bloggingwebsite-backend-a7q5.onrender.com/api/articles/${slug}/comments`
     );
 
     //   console.log("getCurrentUser", { data });
