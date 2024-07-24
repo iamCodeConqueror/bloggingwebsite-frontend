3d683a52a0b9d2048c3a8e92f4aee70392e241c9
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 13:26:29 +0530

    Update useCreateComment.js

--- src/hooks/useCreateComment.js
+++ src/hooks/useCreateComment.js
@@ -9,7 +9,7 @@ const createCommentApi = async (values) => {
     console.log("creteArticle", { values });
 
     const { data } = await axios.post(
-        `http://localhost:3001/api/articles/${values.slug}/comments`,
+        `https://bloggingwebsite-backend-a7q5.onrender.com/api/articles/${values.slug}/comments`,
         { ...values.values }
     );
 
